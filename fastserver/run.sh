8668220d15c5d56494513d90aee2745b5afe648e
daniel-boe <dboe@nhgh.org>
2023-06-11 21:24:18 -0700

    update shell script

--- fastserver/run.sh
+++ fastserver/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Starting home server...'
-cd $HOME/iot-server
+cd $HOME/iot-server/fastserver
 source venv/bin/activate
-uvicorn fastserver.main:app --host 0.0.0.0
+uvicorn main:app --host 0.0.0.0
 
